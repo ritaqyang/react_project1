fa2b8574d4a0cfca92e40cc17193167c044d8787
ritaqyang <ritaqyang@gmail.com>
2023-06-19 12:50:36 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles.css"
+
+
+export default function App(){
+  return (
+    <>
+     <flex> box</flex>
+     <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">new item</label>
+        <input type="text" id="item"></input>
+       </div>
+     </form> 
+    </>
+  )
+
+}
